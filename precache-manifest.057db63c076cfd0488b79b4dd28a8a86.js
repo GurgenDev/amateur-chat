e5457140d0bb72cd8b97b801f3e96949e2dbcbc8
Gurgen <you@example.com>
2020-04-06 13:44:07 +0400

    Updates

---
+++ precache-manifest.057db63c076cfd0488b79b4dd28a8a86.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4318dca5fa6b3461aa1773b45c110a15",
+    "url": "/amateur-chat/index.html"
+  },
+  {
+    "revision": "62ea435ddac8bc3682d8",
+    "url": "/amateur-chat/static/css/main.bf222b9c.chunk.css"
+  },
+  {
+    "revision": "bf9892e749f921ee8b14",
+    "url": "/amateur-chat/static/js/2.c59694f4.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/amateur-chat/static/js/2.c59694f4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "62ea435ddac8bc3682d8",
+    "url": "/amateur-chat/static/js/main.ab490dce.chunk.js"
+  },
+  {
+    "revision": "ac289981a7133ae8e88c",
+    "url": "/amateur-chat/static/js/runtime-main.a80b7b11.js"
+  },
+  {
+    "revision": "73a696ab21efb8c4c3bbaa2101173a0e",
+    "url": "/amateur-chat/static/media/conversation.73a696ab.svg"
+  },
+  {
+    "revision": "ad94eca3da6cb8dcb4a90b412a52a051",
+    "url": "/amateur-chat/static/media/geo.ad94eca3.svg"
+  },
+  {
+    "revision": "f1db42a240ab7bee2d8948a296a0daa0",
+    "url": "/amateur-chat/static/media/loader.f1db42a2.svg"
+  },
+  {
+    "revision": "a92d292aeeff77c95b52bd8fb5b1cbb5",
+    "url": "/amateur-chat/static/media/network.a92d292a.svg"
+  },
+  {
+    "revision": "62b9c530c4a386e80ecc703d967ec644",
+    "url": "/amateur-chat/static/media/research.62b9c530.svg"
+  }
+]);
